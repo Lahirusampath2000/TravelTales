eda86f5dffa9c250aacbc8ef98367e651ef5eb2a
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-04-25 12:34:56 +0530

    connect database and initialize server

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,3 +3,23 @@ import mysql from 'mysql'
 import cors from 'cors'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+dotenv.config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8081")
+});
