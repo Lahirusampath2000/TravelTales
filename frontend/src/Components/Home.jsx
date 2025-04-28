50735d110fd3a665da18119b6d79dc8f3ad591a3
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-04-28 22:27:52 +0530

    added home intial home page

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+            <h1>Welcome to Travel Tales</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
