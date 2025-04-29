5a435b9b0e95738b7af05262058094a2374d5371
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-04-29 21:00:38 +0530

    home page route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
+import Home from './Components/Home';
 
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/login" element={<Login/>} />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Home/>} />
         </Routes>
       </Router>
       
