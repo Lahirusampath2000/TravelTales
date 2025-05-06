0dcc9cfbd568a4479c7f23c968562f1d3d0c91d2
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-05-06 11:49:36 +0530

    create simple dashboard component

---
+++ frontend/src/Components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+            <h1>Welcome to the Dashboard</h1>
+            
+           
+        </div>
+    );
+};
+
+export default Dashboard;
