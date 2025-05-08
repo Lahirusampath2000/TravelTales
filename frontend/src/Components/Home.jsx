5bf3407d88bcaec29e229736d8de9ca3cb853b97
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-05-08 11:31:56 +0530

    latest post div with seperator

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,16 +3,37 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Welcome to Travel Tales</h1>
-      <p>Your next adventure begins here!</p>
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        <h1>Welcome to Travel Tales</h1>
+        <p>Your next adventure begins here!</p>
+      </div>
+      
+      <div style={{ 
+        display: 'flex',
+        alignItems: 'center',
+        margin: '20px 0'
+      }}>
+        <hr style={{
+          flexGrow: 1,
+          border: 'none',
+          borderTop: '1px solid grey',
+          margin: 0
+        }} />
+        <span style={{
+          padding: '0 10px',
+          color: '#666',
+          fontSize: '1.2em'
+        }}>
+          Latest posts
+        </span>
+        <hr style={{
+          flexGrow: 1,
+          border: 'none',
+          borderTop: '1px solid grey',
+          margin: 0
+        }} />
+      </div>
     </div>
-    <br></br>
-    <div>
-      <h2>Latest posts</h2>
-    </div>
-  </div>
-    
   );
 };
 
