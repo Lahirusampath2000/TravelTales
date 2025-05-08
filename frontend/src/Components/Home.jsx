09d8db3f34a94d29e5ce6a89c4cf300752dc3b98
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-05-08 11:15:48 +0530

    added latest post div

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Home = () => {
   return (
+    <div>
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Welcome to Travel Tales</h1>
       <p>Your next adventure begins here!</p>
     </div>
+    <br></br>
+    <div>
+      <h2>Latest posts</h2>
+    </div>
+  </div>
+    
   );
 };
 
